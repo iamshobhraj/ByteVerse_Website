d342b12a64076752ffced5d16c9e6fe908cf2019
Abhinav Singh <115495568+Abhinav1341@users.noreply.github.com>
2024-01-21 00:05:03 +0530

    Update Page.js

--- byteverse_24/src/app/page.js
+++ byteverse_24/src/app/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Byteverse from "";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800 ">
