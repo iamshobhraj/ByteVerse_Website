23ce4ed9a9abd350bfdc23a11e259bad2a5999d9
Abhinav <abhinavsinghhg@gmail.com>
2024-01-23 22:07:45 +0530

    Base Structure Done

---
+++ byteverse_24/src/app/HomePage/page.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <main className="bg-[#ffd4d4]">
+      <Navbar />
+      <div className="h-48 w-full my-10 shadow-xl bg-[#ffd4d4] shadow-black">
+        <h1 className="text-4xl text-slate-700 items-center text-center">
+          Hero Section
+        </h1>
+      </div>
+      <div className="h-48 w-full my-10 shadow-xl bg-[#ffd4d4]  shadow-black">
+        <h1 className="text-4xl text-slate-700 items-center text-center">
+          About Us Section
+        </h1>
+      </div>
+      <div className="h-48 w-full my-10 shadow-xl bg-[#ffd4d4]  shadow-black">
+        <h1 className="text-4xl text-slate-700 items-center text-center">
+          PaperRoll Section
+        </h1>
+      </div>
+      <div className="h-48 w-full my-10 shadow-xl bg-[#ffd4d4]  shadow-black">
+        <h1 className="text-4xl text-slate-700 items-center text-center">
+          Judges Section
+        </h1>
+      </div>
+      <div className="h-48 w-full my-10 shadow-xl bg-[#ffd4d4]  shadow-black">
+        <h1 className="text-4xl text-slate-700 items-center text-center">
+          Footer
+        </h1>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
--- byteverse_24/src/app/page.js
+++ byteverse_24/src/app/page.js
@@ -1,9 +1,10 @@
 import React from "react";
+import HomePage from "./HomePage/page";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800 ">
-      <img src="/byteverse/byteverse.svg"></img>
-    </div>
+    <main>
+      <HomePage />
+    </main>
   );
 }
