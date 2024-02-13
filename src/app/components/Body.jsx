a374906fe3aa93e3b0f2f528500f271d7a34e20e
Abhinav Singh <115495568+Abhinav1341@users.noreply.github.com>
2024-02-13 06:10:13 +0530

    Added Countdown Section in the Hero Section (also for the mobile device) (#43)
    * Added PaperRoll Section
    
    * Bg color changed to match the website
    
    * deleted byteverse_24
    
    * Added id='home'
    
    * countdown added
    
    * suppress hydration warnings in counter
    
    * countdown updated for mobile
    
    ---------
    
    Co-authored-by: Ritwik Singh <itsritwik26@gmail.com>

--- src/app/components/Body.jsx
+++ src/app/components/Body.jsx
@@ -1,38 +1,45 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
-import PaperRoll from '@/../public/PaperRoll.svg'
+import PaperRoll from "@/../public/PaperRoll.svg";
+import CountdownUI from "./countdownUI";
 
 export default function Body() {
   return (
     <motion.div
       initial={{ y: 500 }}
       animate={{ y: 0 }}
-      transition={{ duration: 0.8, delay: 4, type: 'spring', bounce: 0.17 }}
-      className="flex flex-col gap-6 z-40 xl:w-3/4 w-4/5 lg:text-lg text-sm font-lato mt-3">
-      <p className="text-center">
+      transition={{ duration: 0.8, delay: 4, type: "spring", bounce: 0.17 }}
+      className="flex flex-col gap-6 z-40 xl:w-3/4 w-4/5 lg:text-lg text-sm font-lato mt-3"
+    >
+      {/* <p className="text-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet
         gravida mi, sed bibendum diam fermentum a. Proin commodo enim sed augue
         rutrum imperdiet. Aliquam quis aliquam mi. Quisque vehicula porta
         bibendum. Nam vestibulum egestas orci, sit amet semper arcu imperdiet
         nec. Pellentesque facilisis justo vitae dui congue congue.
-      </p>
-      <div className="flex gap-6 items-center justify-center">
-      <div 
-	      className="apply-button" 
-	      data-hackathon-slug="byteversenitp" 
-	      data-button-theme="light"
-	      style={{height: '44px', width: '312px'}}
-      ></div>
-        <a href="path to the file" download className="p-3 text-black font-semibold font-japanese text-2xl" style={{
-          backgroundImage: `url(${PaperRoll.src})`,
-          width: 'fit',
-          height: '100%',
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          
-          }}>
+      </p> */}
+      <div className="flex flex-col gap-6 items-center justify-center">
+        {/* <div
+          className="apply-button"
+          data-hackathon-slug="byteversenitp"
+          data-button-theme="light"
+          style={{ height: "44px", width: "312px" }}
+        ></div> */}
+        <CountdownUI />
+        <a
+          href="path to the file"
+          download
+          className="p-3 text-black font-semibold font-japanese text-xl md:text-2xl"
+          style={{
+            backgroundImage: `url(${PaperRoll.src})`,
+            width: "fit",
+            height: "100%",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
           RULEBOOK
         </a>
       </div>
