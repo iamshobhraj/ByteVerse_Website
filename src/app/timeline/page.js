6246aea43ea66c33bf0de71c91caa0b4eafa01fe
shobh raj <iamshobhraj@gmail.com>
2024-02-15 10:08:13 +0530

    added countdown to timeline (#57)
    * added teams section
    
    * added teams title and foxed judges scroll
    
    * added countdownt to timeline

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -2,11 +2,13 @@ import React from "react";
 import FooterSection from "../components/footer";
 import TimelineHero from "../components/TimelineHero";
 import Schedule from "../components/Schedule";
+import CountdownUI from "../components/countdownUI";
 
 export default function Home() {
   return (
     <main>
       <TimelineHero />
+      <CountdownUI />
       <Schedule />
       <FooterSection />
     </main>
