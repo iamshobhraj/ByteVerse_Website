cad2384d2cab4d3e01c19218dd029956bf1e0334
iamshobh <iamshobhraj@gmail.com>
2024-02-17 01:18:30 +0530

    fixed faq section size

--- src/app/components/FAQ.js
+++ src/app/components/FAQ.js
@@ -41,12 +41,12 @@ const FAQ = () => {
       <div className="absolute inset-0 z-0 flex justify-center items-center">
         <Image src={"/byteverse/sword.png"} height={500} width={500} />
       </div>
-      <div className="z-10 w-full max-w-[1000px] h-auto flex flex-col md:flex-row justify-evenly items-center  p-5">
-        <div className="w-full p-4  md:w-[300px]  flex flex-col justify-center items-start mb-6 md:mb-0 text-[#DEB889]  ">
-          <h2 className="text-3xl mb-6 font-bold ">
+      <div className="z-10 w-full h-auto flex flex-col md:flex-row justify-evenly items-center md:p-48 p-6">
+        <div className="w-full p-4  md:w-[600px]  flex flex-col justify-center items-start mb-6 md:mb-0 text-[#DEB889]  ">
+          <h2 className="text-5xl mb-6 font-bold ">
             Frequently Asked Questions
           </h2>
-          <p className="text-sm">
+          <p className="text-md w-3/4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi natus,
             dignissimos at nostrum illum explicabo distinctio, sapiente, dolores
             nihil magnam praesentium? Ipsam et facilis alias! Temporibus nam hic
