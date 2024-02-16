3af770145cfc04931e5dc75a226c74301ffad5cf
Abhinav <abhinavsinghhg@gmail.com>
2024-02-17 01:52:27 +0530

    minor changes to Prizes

--- src/app/components/Prizes.jsx
+++ src/app/components/Prizes.jsx
@@ -4,7 +4,12 @@ import PrizeCard from "./PrizeCard";
 
 const Prizes = () => {
   return (
-    <div className="relative h-fit pt-24" style={{background:"linear-gradient(180deg,#000000 ,#1f0000 30%,#4a0000 100%)"}}>
+    <div
+      className="relative h-fit pt-24"
+      style={{
+        background: "linear-gradient(180deg,#000000 ,#1f0000 30%,#4a0000 100%)",
+      }}
+    >
       <div className=" inset-0 z-10 flex justify-center items-center">
         <div className="   flex flex-col justify-center items-center z-10 lg:py-10  ">
           <Image
@@ -14,7 +19,7 @@ const Prizes = () => {
             draggable={false}
             className=""
             alt="Knife"
-            style={{scale:"80%"}}
+            style={{ scale: "80%" }}
           />
 
           <div>
@@ -45,13 +50,13 @@ const Prizes = () => {
                 title="Second"
               />
               <div className="md:mb-32 md:mx-20">
-              <PrizeCard
-                image="/prizes/Gold trophy.png"
-                padding="0"
-                title="First"
-              />
+                <PrizeCard
+                  image="/prizes/Gold trophy.png"
+                  padding="0"
+                  title="First"
+                />
               </div>
-              
+
               <PrizeCard
                 image="/prizes/Bronze trophy.png"
                 padding="20"
@@ -61,29 +66,28 @@ const Prizes = () => {
           </div>
         </div>
       </div>
-     <div className="h-full w-full relative insert-0 top-[-60rem]">
-      <div className=" z-0 absolute top-[-52rem] flex justify-center lg:justify-start items-center">
-        <Image
-          src={"/prizes/ninja.png"}
-          height={280}
-          width={280}
-          style={{  }}
-        />
-      </div>
-      <div className="  lg: absolute w-full z-0  top-[-67rem]">
-      <div className="w-full h-full lg:flex justify-end hidden">
-        <Image
-          src={"/prizes/casttle.svg"}
-          width={300}
-          height={300}
-          style={{  }}
-        />
+      <div className="h-full w-full relative insert-0 top-[-60rem]">
+        <div className=" z-0 absolute top-[-52rem] flex justify-center lg:justify-start items-center">
+          <Image
+            src={"/prizes/ninja.png"}
+            height={280}
+            width={280}
+            style={{}}
+          />
         </div>
+        <div className="  lg: absolute w-full z-0  top-[-67rem]">
+          <div className="w-full h-full lg:flex justify-end hidden">
+            <Image
+              src={"/prizes/casttle.svg"}
+              width={300}
+              height={300}
+              style={{}}
+            />
+          </div>
+        </div>
+        <div className="w-full h-56 bg-black absolute top-[-14rem]"></div>
       </div>
-      <div className="w-full h-56 bg-black absolute top-[-14rem]"></div>
-      </div>
-      </div>
-
+    </div>
   );
 };
 
