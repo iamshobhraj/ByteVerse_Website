714a3c91ecdef60717280c9c7ced4893b2ae56af
Abhinav <abhinavsinghhg@gmail.com>
2024-02-17 16:21:18 +0530

    Archive Section Created

--- src/app/components/AboutHsNew.jsx
+++ src/app/components/AboutHsNew.jsx
@@ -5,6 +5,7 @@ import AboutIamge from "./AboutIamge";
 import Link from "next/link";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
+import Archive from "./archive";
 const AboutUs = (props) => {
   const desco_link = "";
   const desco_insta = "";
@@ -20,7 +21,8 @@ const AboutUs = (props) => {
       <div className=" mt-8">
         <AboutIamge />
       </div>
-      <main className="flex flex-col md:flex-row pt-5 lg:pt-24 mb-4 px:8 md:px-4 lg:px-32">
+      <Archive />
+      <main className="flex flex-col md:flex-row pt-3 lg:pt-16 mb-4 px:8 md:px-4 lg:px-32">
         <div className="flex flex-col items-center mt-2 bg-[#391313cf] rounded-2xl py-8 mx-4">
           <div className="w-full md:w-1/4 mb-4 md:mb-0 h-28 flex flex-col">
             <img
@@ -30,12 +32,11 @@ const AboutUs = (props) => {
           </div>
           <div className="w-full px-4 lg:px-10">
             <p className="md:pt-8 text-white text-justify text-sm md:text-base lg:text-lg">
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
+              Hackslash Developer's Club NIT Patna established with the vision
+              of fostering a culture of innovation and collaboration. Hackslash
+              is a dynamic community of developers, designers and tech
+              enthusiasts dedicated to pushing the boundaries of technology and
+              making a positive impact through innovative solutions.
             </p>
           </div>
           <div className="w-full flex flex-row justify-center items-center gap-5 pt-6">
@@ -60,12 +61,11 @@ const AboutUs = (props) => {
           </div>
           <div className="w-full px-4 lg:px-10">
             <p className="md:pt-8 text-white text-justify text-sm md:text-base lg:text-lg">
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
+              GDSC NIT Patna is a hub for innovation and community building
+              where student can learn, connect and grow their skills through
+              various projects, events and mentorship programs. GDSC aim to
+              empower students to develop real- world solutions using Google
+              technologies.
             </p>
           </div>
           <div className="w-full flex flex-row justify-center items-center gap-5 pt-6">
@@ -84,18 +84,17 @@ const AboutUs = (props) => {
         <div className="flex flex-col items-center mt-2 bg-[#391313cf] rounded-2xl py-8 mx-4">
           <div className="w-full md:w-1/4 mb-4 md:mb-0 h-28 flex flex-col">
             <img
-              className="mx-auto -mt-16 md:mt-6 md:scale-[1.2] scale-[0.34] lg:scale-[1]"
+              className="mx-auto -mt-20 lg:mt-4 md:mt-2 md:scale-[1.06] scale-[0.24] lg:scale-[1]"
               src="/byteverse/desco.png"
             />
           </div>
           <div className="w-full px-4 lg:px-10">
             <p className="md:pt-8 text-white text-justify text-sm md:text-base lg:text-lg">
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
-              Lorem ipsum dolor sit amying on meaningful content. Lorem ipsum
-              may be used as a placeholder before final copy is available.
-              adipiscing elit. consectetur adipiscing elit. ... used as a placeh
+              The premier Design Company at the NIT Patna where creativity meets
+              innovation to shape the future.Desco is dedicated to nurturing a
+              community of designers, thinkers and creators. Desco provides
+              real-world experience through workshops, design challenges and
+              collaborative projects.
             </p>
           </div>
           <div className="w-full flex flex-row justify-center items-center gap-5 pt-6">
