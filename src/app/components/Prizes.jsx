a978dc99c4795a5953460bf2575f17892bd7f88d
Dev <devsa2067@gmail.com>
2024-02-24 23:21:42 +0530

    gradiant updated

--- src/app/components/Prizes.jsx
+++ src/app/components/Prizes.jsx
@@ -5,13 +5,13 @@ import PrizeCard from "./PrizeCard";
 const Prizes = () => {
   return (
     <div
-      className="relative h-fit pt-24"
+      className="relative h-fit pt-28"
       style={{
         background: "linear-gradient(180deg,#000000 ,#1f0000 30%,#4a0000 100%)",
       }}
     >
       <div className=" inset-0 z-10 flex justify-center items-center">
-        <div className="   flex flex-col justify-center items-center z-10 lg:py-10  ">
+        <div className="   flex flex-col justify-center items-center  z-10 lg:py-10  ">
           <Image
             src="/prizes/prize.png"
             width={1100}
@@ -85,7 +85,6 @@ const Prizes = () => {
             />
           </div>
         </div>
-        <div className="w-full h-56 bg-black absolute top-[-14rem]"></div>
       </div>
     </div>
   );
