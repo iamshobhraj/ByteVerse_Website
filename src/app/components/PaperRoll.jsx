be0e2773ce3c04c53f3621480395460591d94f2b
Abhinav <abhinavsinghhg@gmail.com>
2024-02-26 21:52:11 +0530

    Added Rulebook

--- src/app/components/PaperRoll.jsx
+++ src/app/components/PaperRoll.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from "next/link";
 import React from "react";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
@@ -20,10 +20,10 @@ const PaperRoll = () => {
 
   return (
     <section id="rules" className="py-8 bg-[#4A0000] flex justify-center">
-      <div className=" py-0 lg:py-20 pb-20 lg:pb-40 flex flex-col align-middle w-[90vw] ">
+      <div className=" py-0 lg:mt-32 lg:py-24 pb-20 lg:pb-40 flex flex-col align-middle w-[90vw] ">
         <img
           src="/PaperRoll.svg"
-          className="absolute w-11/12 left-1/2 -translate-x-1/2 z-0 hidden lg:block"
+          className="absolute w-11/12 left-1/2 -translate-x-1/2 z-0 hidden xl:block"
         ></img>
         <div className="flex flex-row justify-center px-6 md:px-24 lg:px-60 pt-[6rem]">
           <motion.img
@@ -53,9 +53,25 @@ const PaperRoll = () => {
             transition={{ loop: Infinity, duration: 0.1 }}
           />
         </div>
-        <div className="lg:text-lg md:text-2xl lg:text-opacity-95 text-opacity-75 text-cl font-semibold lg:pt-4 pt-16 text-center z-20 text-white lg:text-[#350909] w-3/5 m-auto">
-          <b className="pb-2"> Themes for the Hackathon are: </b>
-          <div className="grid sm:grid-cols-2 md:grid-cols-3">
+        <div className="lg:text-lg md:text-2xl lg:text-opacity-95 text-opacity-75 text-cl font-semibold lg:pt-4 pt-16 text-center z-20 opacity-95 xl:opacity-100 text-slate-100 xl:text-[#350909] w-3/5 m-auto">
+          <p className="pb-2 pt-4 text-xl lg:text-2xl xl:text-3xl font-japanese tracking-wider">
+            {" "}
+            Rulebook:{" "}
+          </p>
+          <Link
+            href="https://bit.ly/byteverse24rulebook"
+            className="pt-4"
+            legacyBehavior
+          >
+            <a
+              className="text-xl lg:text-2xl xl:text-3xl border lg:border-2 xl:border-4 border-slate-100 xl:border-[#4A0000] rounded-xl font-bold mt-4 py-2 px-4 inline-block"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click Here
+            </a>
+          </Link>
+          {/* <div className="grid sm:grid-cols-2 md:grid-cols-3">
             <div className="py-4 pt-0">Quality Education</div>
             <div className="py-4 pt-0">Food Quality and Hunger</div>
             <div className="py-4 pt-0">Cybersecurity</div>
@@ -67,7 +83,7 @@ const PaperRoll = () => {
             <div className="py-4 pt-0">Disaster Management</div>
             <div className="py-4 pt-0">Space and Astrophysics</div>
             <div className="py-4 pt-0">Open Innovation</div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
